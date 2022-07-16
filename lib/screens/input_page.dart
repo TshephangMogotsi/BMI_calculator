2b951e1b061f6f3d991edf3c0bbe4b0037d94766
TshephangMogotsi <zinjie7@gmail.com>
2022-07-16 14:40:49 +0200

    added male and female card with icons

--- lib/screens/input_page.dart
+++ lib/screens/input_page.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+const bottomContainerHeight = 80.0;
+const activeCardColor = Color(0xFF1D1E33);
+const bottomContainerColor = Color(0xFFEB1555);
 
 class InputPage extends StatefulWidget {
   const InputPage({Key? key}) : super(key: key);
@@ -20,39 +25,83 @@ class _InputPageState extends State<InputPage> {
             child: Row(
               children: const[
                 Expanded(
-                  child: ReusableCard(colour: Color(0xFF1D1E33)),
+                  child: ReusableCard(
+                    colour: activeCardColor,
+                    cardChild: CardChildContent(
+                      icon: FontAwesomeIcons.mars,
+                      cardLabel: "MALE",
+                    ),
+                  ),
                 ),
                 Expanded(
-                  child: ReusableCard(colour:  Color(0xFF1D1E33)),
+                   child: ReusableCard(
+                    colour: activeCardColor,
+                    cardChild: CardChildContent(
+                      icon: FontAwesomeIcons.venus,
+                      cardLabel: "FEMALE",
+                    ),
+                  ),
                 ),
               ],
             ),
           ),
           const Expanded(
-            child: ReusableCard(colour: Color(0xFF1D1E33)),
+            child: ReusableCard(colour: activeCardColor),
           ),
           Expanded(
             child: Row(
               children: const <Widget>[
                 Expanded(
-                  child: ReusableCard(colour: Color(0xFF1D1E33)),
+                  child: ReusableCard(colour: activeCardColor),
                 ),
                 Expanded(
-                  child: ReusableCard(colour: Color(0xFF1D1E33)),
+                  child: ReusableCard(colour: activeCardColor),
                 ),
               ],
             ),
           ),
+          Container(
+            color: bottomContainerColor,
+            margin: const EdgeInsets.only(top: 10.0),
+            height: bottomContainerHeight,
+          )
         ],
       ),
     );
   }
 }
 
-class ReusableCard extends StatelessWidget {
-  const ReusableCard({Key? key, required this.colour}) : super(key: key);
-  final Color colour;
+class CardChildContent extends StatelessWidget {
+  const CardChildContent({
+    Key? key,required this.icon,required this.cardLabel
+  }) : super(key: key);
+  final IconData icon;
+  final String cardLabel;
 
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children:  <Widget>[
+        Icon(
+          icon,
+          size: 80.0,
+        ),
+        const SizedBox(height: 15.0),
+        Text(cardLabel,style: const TextStyle(
+          fontSize: 18.0,
+          color:  Color(0xFF8D8E98)
+        ),)
+      ],
+    );
+  }
+}
+
+class ReusableCard extends StatelessWidget {
+  const ReusableCard({Key? key, required this.colour, this.cardChild})
+      : super(key: key);
+  final Color colour;
+  final Widget? cardChild;
 
   @override
   Widget build(BuildContext context) {
@@ -62,6 +111,7 @@ class ReusableCard extends StatelessWidget {
         color: colour,
         borderRadius: BorderRadius.circular(10.0),
       ),
+      child: cardChild,
     );
   }
 }
